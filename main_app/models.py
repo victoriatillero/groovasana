04040620776414cd1cde2cc0cc992693bdd38b12
Victoria Tillero <victoriatillero6@gmail.com>
2025-07-22 00:23:33 -0400

    django models, todo card layout + styling

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+        name = models.CharField(max_length=30)
+        description = models.TextField()
+        day = models.DateField()
+        priority = models.CharField(max_length=20)
+        tags = models.CharField(max_length=50 )
+        esttime =models.DurationField()
+
+        def __str__(self):
+                return self.name
